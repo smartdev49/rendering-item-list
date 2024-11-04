91ef2d91d9115064ea83569f897f38f53a1315d4
unknown <curtisjowen@gamil.com>
2024-11-03 19:15:21 -0500

    fix

--- App.tsx
+++ App.tsx
@@ -3,9 +3,7 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import styles from './styles';
 const data = new Array(100)
   .fill(null)
-  .map((value, index) => ({
-    key: index.toString(), value: `Item $(index)`
-  }));
+  .map((v, i) => ({ key: i.toString(), value: `Item ${i}` }));
 export default function App() {
   return (
     <View style={styles.container}>
