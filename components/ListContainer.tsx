7f2882ef443363063b23b5c63883492ef08b973b
unknown <curtisjowen@gamil.com>
2024-11-05 20:08:54 -0500

    fetch data

--- components/ListContainer.tsx
+++ components/ListContainer.tsx
@@ -1,40 +1,59 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import List from "./List";
+import { fetchItems } from "../api";
+
+type MappedList = {key: string, value: string}[];
 
 function mapItems(items: string[]) {
     return items.map((value, i) => ({key:i.toString(), value}));
 }
 
-const array = new Array(100).fill(null).map((v, i) => `Item ${i}`);
+// const array = new Array(100).fill(null).map((v, i) => `Item ${i}`);
 
-function filterAndSort(text: string, asc: boolean): string[] {
-    return (
-        array
-        .filter((i) => text.length === 0 || i.includes(text))
-        .sort(
-            asc ? 
-            (a , b) => (a > b ? 1 : a < b ? -1 : 0) : 
-            (a , b) => (a < b ? 1 : a > b ? -1 : 0))
-    ) ;
-}
+// function filterAndSort(text: string, asc: boolean): string[] {
+//     return (
+//         array
+//         .filter((i) => text.length === 0 || i.includes(text))
+//         .sort(
+//             asc ? 
+//             (a , b) => (a > b ? 1 : a < b ? -1 : 0) : 
+//             (a , b) => (a < b ? 1 : a > b ? -1 : 0))
+//     ) ;
+// }
 
 export default function ListContainer() {
     const [asc, setAsc] = useState(true);
     const [filter, setFilter] = useState("");
-
-    const data = useMemo(() => {
-        return filterAndSort(filter, asc);
-    }, [filter, asc]);
-
+    const [data, setData]= useState<MappedList>([]);   
+    // const data = useMemo(() => {
+    //     return filterAndSort(filter, asc);
+    // }, [filter, asc]);
+    useEffect(() => {
+        fetchItems(filter, asc)
+            .then((resp) => resp.json())
+            .then(({items}) => {
+                setData(mapItems(items));
+            });
+    }, [])
     return (
         <List
-            data={mapItems(data)}
+            data={data}
             asc={asc}
             onFilter={(text) => {
-                setFilter(text);
+                fetchItems(text, asc)
+                    .then((resp) => resp.json())
+                    .then(({items}) => {
+                        setFilter(text);
+                        setData(mapItems(items));
+                    });
             }}
             onSort={() => {
-                setAsc(!asc);
+                fetchItems(filter, !asc)
+                    .then((resp) => resp.json())
+                    .then(({items}) => {
+                        setAsc(!asc);
+                        setData(mapItems(items));
+                    });
             }}
         />
     )
